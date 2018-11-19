51649e474b9e91d01de78eb5bcfc6afbb0310f3c
dhszheng <shannon@Shannons-MacBook-Air.local>
2018-11-18 20:35:36 -0800

    first commit

---
+++ MathParser.java
@@ -0,0 +1,36 @@
+import java.awt.*;
+
+import javax.swing.JFrame; 
+import javax.swing.JOptionPane; 
+import javax.swing.JPanel;
+
+
+// **********************  Input the Equation  **********************************
+public class MathParser {
+
+
+	public boolean inputEquation(){ // input equation and handle input errors
+		
+   		String equation = JOptionPane.showInputDialog("equation");
+   		MathEvaluator m = new MathEvaluator(equation);
+   		
+   		
+   		m.addVariable("x", ((0.0)+xb));
+   		m.getValue();
+   		//double value_test = m.getValue();
+   		//System.out.println("is equation null?   " + value_test);
+   		
+   		if (m.getValue()== null){JOptionPane.showMessageDialog (null, "EQUATION STRING NOT INPUT PROPERLY", "EQUATION ERROR", JOptionPane.INFORMATION_MESSAGE);
+ 		
+   		 return false; // equation not input correctly
+   		}// end if statement
+   		
+   		return true;
+		
+	}
+
+    public static void main(String[] args) {
+
+        System.out.println(args[0]); 
+    }
+} 
