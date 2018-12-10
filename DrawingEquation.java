be206795728c94e37c91978bb6268cd6310bae22
laurias <skycloudsunshine1362@gmail.com>
2018-12-09 17:46:29 -0800

    added graph

--- DrawingEquation.java
+++ DrawingEquation.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.util.ArrayList;
 import javax.swing.*;
 
 
@@ -6,6 +8,12 @@ import javax.swing.*;
 public class DrawingEquation extends JComponent {
     private double [] xvalues;
     private double [] yvalues;
+    private double xmin = 0.0;
+    private double xmax = 0.0;
+    private double ymin = 0.0;
+    private double ymax = 0.0;
+    private boolean removeDiscontinuity = false;
+    private ArrayList<Pair> discontinuity = new ArrayList<Pair>(2);
 
     public  DrawingEquation() {
         JPanel panel = new JPanel();
@@ -14,28 +22,25 @@ public class DrawingEquation extends JComponent {
         panel.setVisible(true);
     }
 
+    public void setRemoveDiscontinuity(boolean removal) {
+        removeDiscontinuity = removal;
+    }
+
     public void add_xy_values(double [] xval, double [] yval) {
         xvalues = xval;
         yvalues = yval;
-    }
-
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics2D g1 = (Graphics2D) g;
-        int SCREEN_WIDTH = getWidth() - 20;
-        int SCREEN_HEIGHT = getHeight() - 20;
-
-        /**
-         *  Convert xvalues and yvalues into screen positions
-         */
-        double ymin = 0.0;
-        double ymax = 0.0;
         for(int i = 0; i < yvalues.length; i++) {
             if (ymin > yvalues[i]) {
                 ymin = yvalues[i];
             }
             if (ymax < yvalues[i]) {
                 ymax = yvalues[i];
+            }
+            if (xmin > xvalues[i]) {
+                xmin = xvalues[i];
+            }
+            if (xmax < xvalues[i]) {
+                xmax = xvalues[i];
             }
         }
         if (ymin > 0.0) {
@@ -44,36 +49,93 @@ public class DrawingEquation extends JComponent {
         if (ymax < 0.0) {
             ymax = 0.0;
         }
+    }
+
+    public void add_xy_values(double x0, double x1, double y0, double y1, double [] xval, double [] yval) {
+        xvalues = xval;
+        yvalues = yval;
+        for(int i = 0; i < yvalues.length; i++) {
+            if (ymin > yvalues[i]) {
+                ymin = yvalues[i];
+            }
+            if (ymax < yvalues[i]) {
+                ymax = yvalues[i];
+            }
+            if (xmin > xvalues[i]) {
+                xmin = xvalues[i];
+            }
+            if (xmax < xvalues[i]) {
+                xmax = xvalues[i];
+            }
+        }
+        if (ymin > 0.0) {
+            ymin = 0.0;
+        }
+        if (ymax < 0.0) {
+            ymax = 0.0;
+        }
+        if (ymin < y0) {
+            ymin = y0;
+        }
+        if (ymax > y1) {
+            ymax = y1;
+        }
+    }
+
+    public void addDiscontuity(boolean remove, ArrayList disct) {
+        removeDiscontinuity = remove;
+        discontinuity = disct;
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g1 = (Graphics2D) g;
+        int offset = 10;
+        int SCREEN_WIDTH = getWidth() - offset * 2;
+        int SCREEN_HEIGHT = getHeight() - offset * 2;
+
+        /**
+         *  Convert xvalues and yvalues into screen positions
+         */
+
         double yunit_pixel = SCREEN_HEIGHT / Math.abs(ymax - ymin);
-        int yorigin = (int) Math.round(100.0 * (ymax - 0) * yunit_pixel) / 100;
-        int xorigin = SCREEN_WIDTH / 2;
+        double xunit_pixel = SCREEN_WIDTH / Math.abs(xmax - xmin);
+
+        int xorigin = (int) Math.round((0 - xmin) * xunit_pixel) + offset;
+        int yorigin = SCREEN_HEIGHT - (int) Math.round((0 - ymin) * yunit_pixel) + offset;
 
         // Draw coordinates with lightGray
         g1.setStroke(new BasicStroke(2));
         g1.setColor(Color.lightGray);
-        g1.drawLine(0, yorigin, SCREEN_WIDTH, yorigin);
-        g1.drawLine(SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2, SCREEN_HEIGHT);
+        g1.drawLine(offset, yorigin, SCREEN_WIDTH, yorigin);
+        g1.drawLine(SCREEN_WIDTH / 2, offset, SCREEN_WIDTH / 2, SCREEN_HEIGHT + offset);
         // Label coordinate (0, 0)
         g1.setColor(Color.black);
-        g1.drawString("0", SCREEN_WIDTH / 2 - 7, yorigin + 13);
+        //g1.drawString("0", SCREEN_WIDTH / 2 - 7, yorigin + 13);
 
         // Draw x axis ticks and scaled labeling
         for(int i = 0; i < xvalues.length; i++) {
-            int xpos = i * (SCREEN_WIDTH / xvalues.length);
-            if (i % 5 == 0 ) {
+            if (i % 10 == 0 ) {
+                int xpos = (int) Math.round((xvalues[i] - xmin) * xunit_pixel) + offset;
                 g1.drawLine(xpos, yorigin, xpos, yorigin + 5);
-                //g1.drawString(String.valueOf(xvalues[i]), xpos - 7, yorigin + 13);
+                g1.drawString(String.valueOf((int)xvalues[i]), xpos, yorigin - 10);
             }
         }
 
         // Draw y axis ticks and scaled labeling
-        int num_y_ticks = (int) Math.round(100.0 * SCREEN_HEIGHT / yunit_pixel) / 100;
+        // Use a total of num_y_ticks (default = 10)
+        // A total SCREEN_HEIGHT and
+        int num_y_ticks = 15;
+        double tick_interval = (ymax - ymin) / num_y_ticks;
+        int [] y_tick_values = new int[num_y_ticks];
+        for (int i = 0; i < num_y_ticks; i++) {
+            y_tick_values[i] = (int) (ymin + i * tick_interval);
+        }
+
         for(int j = 0; j < num_y_ticks; j++) {
-            int ypos = SCREEN_HEIGHT - j * (int) Math.round(yunit_pixel * 100)/100;
-            if (j % 5 == 0) {
-                g1.drawLine(SCREEN_WIDTH / 2, ypos, SCREEN_WIDTH / 2 + 5, ypos);
-                //g1.drawString(String.valueOf(yunit_pixel * j), SCREEN_WIDTH / 2 + 7, ypos);
-            }
+            int ypos = SCREEN_HEIGHT - (int)((y_tick_values[j] - ymin) * yunit_pixel) + offset;
+            g1.drawLine(SCREEN_WIDTH / 2, ypos, SCREEN_WIDTH / 2 + 5, ypos);
+            g1.drawString(String.valueOf(y_tick_values[j]), SCREEN_WIDTH / 2 + 20, ypos);
         }
 
 
@@ -90,12 +152,26 @@ public class DrawingEquation extends JComponent {
         //line1
         Polygon p = new Polygon();
         for (int x = 0; x < xvalues.length; x++) {
-            double xpos = x * xscale;
-            double ypos = (ymax - yvalues[x]) * yunit_pixel;
+            //double xpos = x * xscale;
+            //double ypos = (ymax - yvalues[x]) * yunit_pixel;
+            double xpos = (xvalues[x] - xmin) * xunit_pixel + offset;
+            double ypos = SCREEN_HEIGHT - (yvalues[x] - ymin) * yunit_pixel + offset;
             p.addPoint((int)xpos, (int)ypos);
 
         }
         g2.drawPolyline(p.xpoints, p.ypoints, p.npoints);
+
+        if (removeDiscontinuity) {
+            Pair pair = new Pair();
+            for(int i = 0; i < discontinuity.size(); i++)
+                pair = discontinuity.get(i);
+                double xpos = (pair.getX() - xmin) * xunit_pixel + offset / 2;;
+                double ypos = SCREEN_HEIGHT - (pair.getY() - ymin) * yunit_pixel + offset;
+
+                Ellipse2D.Double cir = new Ellipse2D.Double(xpos, ypos, 10, 10);
+                g2.setColor(Color.blue);
+                g2.draw(cir);
+        }
 
     }
 
--- MathEquationGraph.java
+++ MathEquationGraph.java
@@ -1,50 +1,91 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.Arrays;
 import javax.swing.*;
 
 
 public class MathEquationGraph extends JFrame implements ActionListener {
-    private JLabel greeting = new JLabel("Please enter your equation:");
-    private JTextField equation = new JTextField(50);
+    private JLabel greeting = new JLabel("Equation:");
+    private JTextField equation = new JTextField(15);
+    private JTextField denominator = new JTextField(15);
     private JButton graphButton = new JButton("Graph it");
-    private JTextField message = new JTextField(60);
+    private JLabel min_label = new JLabel("Min x value:");
+    private JTextField min_xvalue = new JTextField(8);
+    private JLabel max_label = new JLabel("Max x value:");
+    private JTextField max_xvalue = new JTextField(8);
+    private JLabel ymin_label = new JLabel("Min y value:");
+    private JTextField ymin_value = new JTextField(8);
+    private JLabel ymax_label = new JLabel("Max y value:");
+    private JTextField ymax_value = new JTextField(8);
+    //private JCheckBox rational = new JCheckBox("Rational", false);
+    private JTextField message = new JTextField(50);
     private DrawingEquation graph = new DrawingEquation();
+
+    private JRadioButton asis = new JRadioButton("As is");
+    private JRadioButton der1 = new JRadioButton("First derivative");
+    private JRadioButton der2 = new JRadioButton("Second derivative");
+    private JRadioButton rational = new JRadioButton("Rational");
+    private ButtonGroup bGroup = new ButtonGroup();
+
 
     // layout
     public MathEquationGraph() {
         // layout components
+        /**
+         * Make 2 JPanels. Top Panel for input. Bottom Panel for plot.
+         * Put top panel at North and plot panel at Center using BorderLayout
+         */
         JPanel topPanel = new JPanel();
+        topPanel.setLayout(new BorderLayout());
+        JPanel p1 = new JPanel();
+        JPanel p2 = new JPanel();
 
-        GridBagLayout gridbag = new GridBagLayout();
-        GridBagConstraints c = new GridBagConstraints();
-        topPanel.setLayout(gridbag);
-        c.fill = GridBagConstraints.BOTH;
-        c.weightx = 1.0;
-        gridbag.setConstraints(greeting, c);
-        topPanel.add(greeting);
-        gridbag.setConstraints(equation, c);
-        topPanel.add(equation);
-        String default_equation = "x * x - 10 * x + 6";
+        p1.setLayout(new FlowLayout());
+        p2.setLayout(new FlowLayout());
+
+        p1.add(greeting);
+        p1.add(equation);
+        String default_equation = "x^2 - 7*x + 12";
         equation.setText(default_equation);
-        c.gridwidth = GridBagConstraints.REMAINDER; //end of row
-        gridbag.setConstraints(graphButton, c);
-        topPanel.add(graphButton);
+        bGroup.add(asis);
+        bGroup.add(der1);
+        bGroup.add(der2);
+        bGroup.add(rational);
+        p1.add(asis);
+        p1.add(der1);
+        p1.add(der2);
+        p1.add(rational);
+        p1.add(denominator);
 
-        c.weightx = 0.0;                      //reset to the default
-        gridbag.setConstraints(message, c);   // add row
-        topPanel.add(message);
+        p2.add(min_label);
+        p2.add(min_xvalue);
+        min_xvalue.setText("-10");
+        p2.add(max_label);
+        p2.add(max_xvalue);
+        max_xvalue.setText("10");
+        p2.add(ymin_label);
+        p2.add(ymin_value);
+        p2.add(ymax_label);
+        p2.add(ymax_value);
+
+        p2.add(graphButton);
+
+        topPanel.add(p1, BorderLayout.NORTH);
+        topPanel.add(p2, BorderLayout.CENTER);
+        topPanel.add(message, BorderLayout.SOUTH);
 
         getContentPane().add(topPanel, BorderLayout.NORTH);
 
         /*
-        Draw the default equation with 100 poins
+        Draw the default equation with 100 points
          */
-        int npoints = 100;
         int xmin = -10;
         int xmax = 10;
-        double xunit = (double) (xmax - xmin) / (double) (npoints + 1);
+        int npoints = 10 * (xmax - xmin + 1);
+        double xunit = (double) (xmax - xmin + 1) / npoints;
+
         double [] xvalues = new double[npoints];
         double [] yvalues = new double[npoints];
         MathEvaluator m = new MathEvaluator(default_equation);
@@ -54,7 +95,6 @@ public class MathEquationGraph extends JFrame implements ActionListener {
             xvalues[i] = x;
             yvalues[i] = Double.valueOf(m.getValue());
         }
-        //message.setText(Arrays.toString(yvalues));
 
         //DrawingEquation graph = new DrawingEquation();
         graph.add_xy_values(xvalues, yvalues);
@@ -69,31 +109,156 @@ public class MathEquationGraph extends JFrame implements ActionListener {
         Object source = event.getSource();
         if (source == graphButton) {
             String equationText = equation.getText();
-            //message.setText(equationValue);
-
-            int xmin = -10;
-            int xmax = 10;
-            int npoints = 100;
-            double xunit = (double) (xmax - xmin) / (double) (npoints + 1);
+            String denomText = denominator.getText();
+            int xmin = Integer.valueOf(min_xvalue.getText());
+            int xmax = Integer.valueOf(max_xvalue.getText());
+            int npoints = 10 * (xmax - xmin + 1);
+            double xunit = (double) (xmax - xmin + 1) / npoints;
             double [] xvalues = new double[npoints];
             double [] yvalues = new double[npoints];
-            MathEvaluator m = new MathEvaluator(equationText);
-            for (int i = 0; i < npoints; i++) {
-                double x = xmin + xunit * i;
-                m.addVariable("x", x);
-                xvalues[i] = x;
-                yvalues[i] = Double.valueOf(m.getValue());
+            double ymin = 0.0;
+            double ymax = 0.0;
+
+            //reset removeDiscontinuity flag
+            graph.setRemoveDiscontinuity(false);
+
+            boolean isAsis = asis.isSelected();
+            boolean isDer1 = der1.isSelected();
+            boolean isDer2 = der2.isSelected();
+            boolean isRational = rational.isSelected();
+
+            String msg = "As is selected";
+            if (isDer1) {
+                msg = "First derivatie";
             }
-            message.setText(equationText);
-            graph.add_xy_values(xvalues, yvalues);
-            graph.repaint();
+            else if (isDer2) {
+                msg = "Second derivative";
+            }
+            else if (isRational) {
+                msg = "Rational";
+            }
+            message.setText(msg);
             /*
-            MathEvaluator m = new MathEvaluator(equationText);
-            m.addVariable("x", 15.1d);
-            Double eqValue = m.getValue();
-            Double v = m.getVariable("x");
-            message.setText("x=" + v.toString() + ": " + eqValue.toString());
-            */
+            If rational, find out if there is removable discontinuity
+             */
+            if (isRational) {
+                if (denomText.isEmpty()) {
+                    message.setText("Denominator is not provided. Please enter into box next to Rational button");
+                }
+                else {
+                    message.setText("(" + equationText + ") / (" + denomText + ")");
+                    Polynomial numator = new Polynomial(equationText);
+                    Polynomial denom = new Polynomial(denomText);
+
+                    // at which x, denominator == 0
+                    ArrayList<Pair> discont = new ArrayList<Pair>(2);
+                    boolean removeDiscontinuity = false;
+                    for (int x = xmin; x < xmax; x++) {
+                        double y = denom.evaluate(x);
+                        if (Math.abs(y - 0.0) < 0.00001) {
+                            // go to left and right of x to see if equation values are similar
+                            double x1 = x - 0.001;
+                            double x2 = x + 0.001;
+                            double y1 = numator.evaluate(x1) / denom.evaluate(x1);
+                            double y2 = numator.evaluate(x2) / denom.evaluate(x2);
+                            if (Math.abs(y1 - y2) < 1000) {
+                                double y_mean = (y1 + y2) / 2;
+                                discont.add(new Pair(x, y_mean));
+                                removeDiscontinuity = true;
+                                System.out.println("x= " + x + ";    y_mean=" + y_mean);
+                            }
+                        }
+                        System.out.println("x= " + x + ";    y=" + y);
+                    }
+                    if (removeDiscontinuity) {
+                        graph.addDiscontuity(removeDiscontinuity, discont);
+                    }
+
+                    // generate x, y values for plotting
+                    for (int i = 0; i < npoints; i++) {
+                        double x = xmin + xunit * i;
+                        xvalues[i] = x;
+                        double yval = 0;
+                        if (denom.evaluate(x) != 0.0) {
+                            yval = numator.evaluate(x) / denom.evaluate(x);
+                        }
+                        //System.out.println("x= " + x + ";    y=" + yval);
+                        yvalues[i] = yval;
+                        if (ymin > yval) {
+                            ymin = yval;
+                        }
+                        if (ymax < yval) {
+                            ymax = yval;
+                        }
+                    }
+                }
+
+            } else if (isAsis) {
+                message.setText("Equation: " + equationText);
+
+                MathEvaluator m = new MathEvaluator(equationText);
+                for (int i = 0; i < npoints; i++) {
+                    double x = xmin + xunit * i;
+                    m.addVariable("x", x);
+                    xvalues[i] = x;
+                    double yval = Double.valueOf(m.getValue());
+                    yvalues[i] = yval;
+                    if (ymin > yval) {
+                        ymin = yval;
+                    }
+                    if (ymax < yval) {
+                        ymax = yval;
+                    }
+                }
+            }
+            else if (isDer1) {
+                Polynomial p = new Polynomial(equationText);
+                Polynomial first_der = p.derivative();
+                message.setText(first_der.toString());
+
+                for (int i = 0; i < npoints; i++) {
+                    double x = xmin + xunit * i;
+                    double yval = first_der.evaluate(x);
+                    xvalues[i] = x;
+                    yvalues[i] = yval;
+                    if (ymin > yval) {
+                        ymin = yval;
+                    }
+                    if (ymax < yval) {
+                        ymax = yval;
+                    }
+                }
+            }
+            else if (isDer2) {
+                Polynomial p = new Polynomial(equationText);
+                Polynomial first_der = p.derivative();
+                Polynomial second_der = first_der.derivative();
+                message.setText(second_der.toString());
+
+                for (int i = 0; i < npoints; i++) {
+                    double x = xmin + xunit * i;
+                    double yval = second_der.evaluate(x);
+                    xvalues[i] = x;
+                    yvalues[i] = yval;
+                    if (ymin > yval) {
+                        ymin = yval;
+                    }
+                    if (ymax < yval) {
+                        ymax = yval;
+                    }
+                }
+            }
+
+
+            if (!ymin_value.getText().isEmpty()) {
+                ymin = Double.valueOf(ymin_value.getText());
+            }
+            if (!ymax_value.getText().isEmpty()) {
+                ymax = Double.valueOf(ymax_value.getText());
+            }
+            graph.add_xy_values(xmin, xmax, ymin, ymax, xvalues, yvalues);
+            graph.repaint();
+
         }
     }
 
